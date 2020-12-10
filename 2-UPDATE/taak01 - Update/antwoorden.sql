7289f300a2cbea46e6ce7d4c5e3cd103c9a5a7de
Lorenzo Breedveld <2105144@talnet.nl>
2020-12-10 12:07:01 +0100

    af

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,12 @@
 -- Opdracht 1
-
+UPDATE studenten2 SET woonplaats = 'Amstelveen'
 -- Opdracht 2 
-
+UPDATE `studenten2` SET`adres`='Lidlweg 9',`postcode`='1191BE' WHERE student_id= 8;
+UPDATE `studenten2` SET`adres`='Lidlweg 10',`postcode`='1230HY' WHERE student_id= 9;
+UPDATE `studenten2` SET`adres`='Lidlweg 11',`postcode`='3367PL' WHERE student_id= 10;
 -- Opdracht 3
-
+UPDATE `studenten2` SET geboortedatum = '2000-12-12' WHERE student_id= 2
 -- Opdracht 4
-
+UPDATE studenten2 SET klas = '9A' WHERE student_id= 8
+UPDATE studenten2 SET klas = '9C' WHERE student_id= 4
 -- Opdracht 5
